b80329650e15b587a5412ea02f74d53a84911e78
Matthias Faust <Fausti@users.noreply.github.com>
2016-05-09 19:14:33 +0200

    onMessage Event in einen Timer gesteckt...

--- stube.emotesv2.plugin.js
+++ stube.emotesv2.plugin.js
@@ -2,6 +2,8 @@
 //test
 function StubeEmotes() {}
 
+var timer;
+
 StubeEmotes.prototype.getName = function() {
     return "Stube Emotes";
 };
@@ -11,11 +13,11 @@ StubeEmotes.prototype.getDescription = function() {
 };
 
 StubeEmotes.prototype.getVersion = function() {
-    return "1.1.0";
+    return "1.1.1";
 };
 
 StubeEmotes.prototype.getAuthor = function() {
-    return "bamboechop, MechaJunk";
+    return "bamboechop, MechaJunk, Valance";
 };
 
 StubeEmotes.prototype.getSettingsPanel = function() {
@@ -23,63 +25,83 @@ StubeEmotes.prototype.getSettingsPanel = function() {
 };
 
 StubeEmotes.prototype.load = function() {
-    console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " loaded.", "color: limegreen; font-weight: bold;", "");
+    // console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " loaded.", "color: limegreen; font-weight: bold;", "");
 };
 
 StubeEmotes.prototype.unload = function() {
-    console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + "unloaded.", "color: limegreen; font-weight: bold;", "");
+    // console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + "unloaded.", "color: limegreen; font-weight: bold;", "");
 };
 
 StubeEmotes.prototype.start = function() {
     var messages = document.querySelectorAll(".message-text > .markup");
     BdApi.getPlugin("Stube Emotes").emotes(messages);
 };
+
 StubeEmotes.prototype.stop = function() {
-    console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " stopped.", "color: red; font-weight: bold;", "");
+    // console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " stopped.", "color: red; font-weight: bold;", "");
 };
 
 StubeEmotes.prototype.onMessage = function() {
-    var messages = $(".message .markup");
-    BdApi.getPlugin("Stube Emotes").emotes(messages);
+    // Sinn? Eigentlich keiner, wird doch eh überschrieben, oder?
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+    
+    timer = setTimeout(function() {
+        // var messages = document.querySelectorAll(".message-text > .markup");
+        var messages = $(".message .markup");
+        
+        BdApi.getPlugin("Stube Emotes").emotes(messages);
+        timer = null;
+    }, 500);
+    
+    // console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " [onMessage] event fired.", "color: purple; font-weight: bold;", "");
 };
 
 StubeEmotes.prototype.onSwitch = function() {
     var messages = document.querySelectorAll(".message-text > .markup");
     BdApi.getPlugin("Stube Emotes").emotes(messages);
+    
+    // console.info("%c[BetterDiscord]" + " %c" + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " [onSwitch] event fired.", "color: purple; font-weight: bold;", "");
 };
 
-var map = { ":nangry:" : '<img class="image" src="https://i.imgur.com/mWWce.png">', 
-            ":n:" : '<img class="image" src="https://i.imgur.com/DLKdPuR.png">',
-            ":ncute:" : '<img class="image" src="https://i.imgur.com/geptw.png">' ,
-            ":nwow:" : '<img class="image" src="https://i.imgur.com/DjCdA.png">',
-            ":nroll:" : '<img class="image" src="https://i.imgur.com/WLIba.gif">',
-            ":nninja:" : '<img class="image" src="https://i.imgur.com/TkTrY.png">',
-            ":nmustocle:" : '<img class="image" src="https://i.imgur.com/Oh5Bi.png">',
-            ":nscience:" : '<img class="image" src="https://i.imgur.com/zqfk4.gif">',
-            ":nsad:" : '<img class="image" src="https://i.imgur.com/SW1WA.gif">',
-            ":3:" : '<img class="image" src="https://i.imgur.com/l1tf4K2.png">',
-            ":ntoot:" : '<img class="image" src="https://i.imgur.com/nGVHr.png">',
-            ":nfakt:" : '<img class="image" src="https://i.imgur.com/A5Vru.png">',
-            ":nrolleyes:" : '<img class="image" src="https://i.imgur.com/JYGVM.gif">',
-            ":p:" : '<img class="image" src="https://i.imgur.com/KG6DQVu.png">',
-            ":luck:" : '<img class="image" src="https://i.imgur.com/SR6vz5s.png">',
-            "._." : '<img class="image" src="https://i.imgur.com/P6SPTuj.gif">',
-            ":3" : '<img class="image" src="https://i.imgur.com/mMJIRoH.gif">',
-            "xD" : '<img class="image" src="https://i.imgur.com/CZ5gcxk.png">',
-            ":U" : '<img class="image" src="https://i.imgur.com/s9Crr5g.gif">',
-            ":kuss:" : '<img class="image" src="https://i.imgur.com/4xwtbbi.gif">',
-            ":oops:" : '<img class="image" src="https://i.imgur.com/rxBUAEb.gif">',
-            ":smug:" : '<img class="image" src="https://i.imgur.com/enEWh1U.gif">',
-            ":sabber:" : '<img class="image" src="https://i.imgur.com/bZgy6Kc.gif">',
-            ":I" : '<img class="image" src="https://i.imgur.com/iTRR5e1.gif">',
-            ":wus:" : '<img class="image" src="https://i.imgur.com/2jFtxIT.gif">',
-            ":eww:" : '<img class="image" src="https://i.imgur.com/DwfFnxq.gif">',
-            ":facepalm:" : '<img class="image" src="https://i.imgur.com/FZKW2aT.gif">',
-            ":mustocle:" : '<img class="image" src="https://i.imgur.com/smw3q79.gif">',
-            ":herp:" : '<img class="image" src="https://i.imgur.com/Ng8iuQl.gif">',
-            ":derp:" : '<img class="image" src="https://i.imgur.com/rQ4lz0p.gif">',
-            ":starr:" : '<img class="image" src="https://i.imgur.com/fXZks3m.png">',
-            ":bambus:" : '<img class="image" src="https://i.imgur.com/7vXpELW.gif">'
+function getImageElement(url) {
+    return '<img draggable="false" class="image" src="' + url + '">';
+}
+
+var map = { ":nangry:" : 'https://i.imgur.com/mWWce.png', 
+            ":n:" : 'https://i.imgur.com/DLKdPuR.png',
+            ":ncute:" : 'https://i.imgur.com/geptw.png' ,
+            ":nwow:" : 'https://i.imgur.com/DjCdA.png',
+            ":nroll:" : 'https://i.imgur.com/WLIba.gif',
+            ":nninja:" : 'https://i.imgur.com/TkTrY.png',
+            ":nmustocle:" : 'https://i.imgur.com/Oh5Bi.png',
+            ":nscience:" : 'https://i.imgur.com/zqfk4.gif',
+            ":nsad:" : 'https://i.imgur.com/SW1WA.gif',
+            ":3:" : 'https://i.imgur.com/l1tf4K2.png',
+            ":ntoot:" : 'https://i.imgur.com/nGVHr.png',
+            ":nfakt:" : 'https://i.imgur.com/A5Vru.png',
+            ":nrolleyes:" : 'https://i.imgur.com/JYGVM.gif',
+            ":p:" : 'https://i.imgur.com/KG6DQVu.png',
+            ":luck:" : 'https://i.imgur.com/SR6vz5s.png',
+            "._." : 'https://i.imgur.com/P6SPTuj.gif',
+            ":3" : 'https://i.imgur.com/mMJIRoH.gif',
+            "xD" : 'https://i.imgur.com/CZ5gcxk.png',
+            ":U" : 'https://i.imgur.com/s9Crr5g.gif',
+            ":kuss:" : 'https://i.imgur.com/4xwtbbi.gif',
+            ":oops:" : 'https://i.imgur.com/rxBUAEb.gif',
+            ":smug:" : 'https://i.imgur.com/enEWh1U.gif',
+            ":sabber:" : 'https://i.imgur.com/bZgy6Kc.gif',
+            ":I" : 'https://i.imgur.com/iTRR5e1.gif',
+            ":wus:" : 'https://i.imgur.com/2jFtxIT.gif',
+            ":eww:" : 'https://i.imgur.com/DwfFnxq.gif',
+            ":facepalm:" : 'https://i.imgur.com/FZKW2aT.gif',
+            ":mustocle:" : 'https://i.imgur.com/smw3q79.gif',
+            ":herp:" : 'https://i.imgur.com/Ng8iuQl.gif',
+            ":derp:" : 'https://i.imgur.com/rQ4lz0p.gif',
+            ":starr:" : 'https://i.imgur.com/fXZks3m.png',
+            ":bambus:" : 'https://i.imgur.com/7vXpELW.gif'
 
 };
 
@@ -88,9 +110,8 @@ StubeEmotes.prototype.emotes = function(messages) {
     forEach(messages, function(index, value) {
         var line = value.innerHTML;
         for (var key in map) {
-            line = line.replaceAll(key, map[key]);
+            line = line.replaceAll(key, getImageElement(map[key]));
         }
-
         value.innerHTML = line;
     });
 }
